db2fe87d07611f4ce8d9d7982f0a6c2b595c58d9
senthil-01 <senthil.work01@gmail.com>
2026-03-25 08:47:32 +0530

    vercel link added

--- main.py
+++ main.py
@@ -94,6 +94,7 @@ app.add_middleware(
         "http://127.0.0.1:8080",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
+        "https://cravel-frontend.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
